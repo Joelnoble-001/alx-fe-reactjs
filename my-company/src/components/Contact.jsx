ff8e7b503976671e556c6995425803a35335acd3
joel odunoye <joelodunoye@gmail.com>
2026-01-18 10:48:28 +0100

    react router for my-company

--- my-company/src/components/Contact.jsx
+++ my-company/src/components/Contact.jsx
@@ -19,7 +19,6 @@ function Contact() {
   return (
     <div style={{ padding: '20px' }}>
       <h1>Contact Us</h1>
-
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -29,7 +28,6 @@ function Contact() {
           onChange={handleChange}
           style={{ display: 'block', margin: '10px 0' }}
         />
-
         <input
           type="email"
           name="email"
@@ -38,7 +36,6 @@ function Contact() {
           onChange={handleChange}
           style={{ display: 'block', margin: '10px 0' }}
         />
-
         <textarea
           name="message"
           placeholder="Your Message"
@@ -46,7 +43,6 @@ function Contact() {
           onChange={handleChange}
           style={{ display: 'block', margin: '10px 0' }}
         />
-
         <button type="submit">Send Message</button>
       </form>
     </div>
