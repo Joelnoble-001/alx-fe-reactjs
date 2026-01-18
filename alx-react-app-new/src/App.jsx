25ec3d2392057fa0ae5e703d8e373a1dc2e91bb5
joel odunoye <joelodunoye@gmail.com>
2026-01-18 08:11:50 +0100

    add more react files

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage';
+
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
+import Counter from './components/Counter';
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    {/* Welcome message component */}
+      <WelcomeMessage />
+
+      {/* Header, MainContent, and Footer components */}
+      <Header />
+      <MainContent />
+      <Footer />
+
+      {/* Example UserProfile component */}
+
+      <UserProfile 
+        name="Alice" 
+        age={25} 
+        bio="Loves hiking and photography" 
+      />
+      
+      {/* Counter component */}
+
+      <div>
+      <h1>React Counter App</h1>
+      <Counter />
+    </div>
+
+     {/* Vite + React logos and counter */}
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+function MainContent() {
+  return (
+    <main style={{
+        padding: '20px',
+        backgroundColor: '#f4f4f4',
+        minHeight: '200px'
+      }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
