bc5d56d61b2bb0098d1423eef7ae2790f32d8227
joel odunoye <joelodunoye@gmail.com>
2026-01-30 09:35:42 +0100

    update

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,17 +1,18 @@
 import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
+  // state
   recipes: [],
-
   searchTerm: '',
-  setSearchTerm: (term) => set({ searchTerm: term }),
+  favorites: [],
 
+  
+  setRecipes: (recipes) => set({ recipes }),
+
+  // actions
   addRecipe: (recipe) =>
-    set((state) => ({ recipes: [...state.recipes, recipe] })),
-
-  deleteRecipe: (id) =>
     set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+      recipes: [...state.recipes, recipe],
     })),
 
   updateRecipe: (updatedRecipe) =>
@@ -19,5 +20,22 @@ export const useRecipeStore = create((set) => ({
       recipes: state.recipes.map((recipe) =>
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
       ),
+    })),
+
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+
+  setSearchTerm: (term) => set({ searchTerm: term }),
+
+  addFavorite: (id) =>
+    set((state) => ({
+      favorites: [...state.favorites, id],
+    })),
+
+  removeFavorite: (id) =>
+    set((state) => ({
+      favorites: state.favorites.filter((fid) => fid !== id),
     })),
 }));
