b4e329df20db1c43dd9b990fdc18116e3b141be7
joel odunoye <joelodunoye@gmail.com>
2026-02-21 15:56:22 +0100

    recipe-sharing-platform

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}",
+      "./public/index.html"
+    ],
     theme: {
       extend: {},
     },
