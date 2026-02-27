c6cce9c3642c5e3d92b76107a84a2b4a14df5168
joel odunoye <joelodunoye@gmail.com>
2026-02-27 12:16:22 +0100

    update files

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -2,26 +2,28 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-// Fetch function
 const fetchPosts = async () => {
   const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
   return response.data;
 };
 
 export default function PostsComponent() {
-  const { data, isLoading, isError, refetch } = useQuery({
+  const { data, isLoading, error, refetch } = useQuery({
     queryKey: ["posts"],
     queryFn: fetchPosts,
-    staleTime: 1000 * 60 * 5, // cache data for 5 minutes
+    staleTime: 1000 * 60 * 5,
   });
 
   if (isLoading) return <p>Loading posts...</p>;
-  if (isError) return <p>Error fetching posts.</p>;
+
+  
+  if (error) return <p>Error fetching posts.</p>;
 
   return (
     <div>
       <h1>Posts</h1>
       <button onClick={refetch}>Refetch Posts</button>
+
       <ul>
         {data.map((post) => (
           <li key={post.id}>
@@ -30,6 +32,7 @@ export default function PostsComponent() {
           </li>
         ))}
       </ul>
+
     </div>
   );
 }
